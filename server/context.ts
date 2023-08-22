fee46491867ae21fc405228f49fe69820503add8
Stephen Paul <stephensimonpaul@gmail.com>
2023-08-22 18:08:33 +0200

    update context to getToken()

--- server/context.ts
+++ server/context.ts
@@ -1,6 +1,6 @@
 import { TRPCError, inferAsyncReturnType } from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
-import { decode } from "next-auth/jwt";
+import { decode, getToken } from "next-auth/jwt";
 import { prisma } from './routers/_app';
 import { UserId } from './dtos';
 
@@ -9,10 +9,17 @@ const secret = process.env.NEXTAUTH_SECRET!
 export async function createContext({
   req,
 }: trpcNext.CreateNextContextOptions) {
-  const sessionToken = req.cookies['next-auth.session-token'];
+  // const token = await getToken({req});
+  // console.log('token', token);
+  // const sessionToken = req.cookies['next-auth.session-token'];
+  // if (!sessionToken) { throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Session Token not found' }); }
+  // const user = await decode({ token: sessionToken, secret });
+  // const userFromDb = await prisma.user.findFirst({ where: { email: user!.email! } });
+  // if (!userFromDb) { throw new Error('User not found in database') }
+  // return { userId: userFromDb.id as UserId };
+  const sessionToken = await getToken({ req });
   if (!sessionToken) { throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Session Token not found' }); }
-  const user = await decode({ token: sessionToken, secret });
-  const userFromDb = await prisma.user.findFirst({ where: { email: user!.email! } });
+  const userFromDb = await prisma.user.findFirst({ where: { email: sessionToken.email! } });
   if (!userFromDb) { throw new Error('User not found in database') }
   return { userId: userFromDb.id as UserId };
 }
