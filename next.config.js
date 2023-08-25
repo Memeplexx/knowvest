c65c38afe485e37dcdfd424e0f68048e2f8cd786
Stephen Paul <stephensimonpaul@gmail.com>
2023-08-25 22:16:41 +0200

    Update to support PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA({
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
@@ -29,6 +36,4 @@ const nextConfig = {
     // config.devtool = 'eval-cheap-module-source-map';
     return config
   },
-}
-
-module.exports = nextConfig
+})
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head
+        children={
+          <>
+            <link rel="manifest" href="/manifest.json" />
+            <link rel="apple-touch-icon" href="/icon.png" />
+            <meta name="theme-color" content="#000" />
+          </>
+        }
+      />
       <body>
         <Main />
         <NextScript />
