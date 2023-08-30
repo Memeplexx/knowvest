8413f7efd38205797df4955fc68d597a39d141ac
Stephen Paul <stephensimonpaul@gmail.com>
2023-08-30 15:25:49 +0200

    Implement skeleton loaders

--- components/loader-skeleton/index.tsx
+++ components/loader-skeleton/index.tsx
@@ -1,23 +1,14 @@
 import ContentLoader from 'react-content-loader'
-import { Container } from './styles';
 
 const LoaderSkeleton = (props: { count: number } = { count: 10 }) => {
   const rows = new Array(props.count).fill(null).map((_, i) => i);
-  // const [rows, setRows] = useState(new Array<number>());
-  // const ref = useRef<HTMLDivElement | null>(null);
-  // useIsomorphicLayoutEffect(() => {
-  //   // console.log('......', );
-  //   // const count = Math.floor(ref.current!.offsetHeight / 40);
-  //   // console.log(count);
-  //   const count = 15;
-  //   setRows(new Array(count).fill(null).map((_, i) => i))
-  // }, [])
   return (
-    <Container
-      // ref={ref}
+    <div
+      style={{width: '100%', height: '100%'}}
       children={rows.map(key => (
         <ContentLoader
-          // {...props}
+          uniqueKey={`loader-${key}`}
+          {...props}
           key={key}
           height={40}
           backgroundColor={`rgba(255, 255, 255, ${(1 - (key / props.count)) / 2})`}
@@ -25,9 +16,9 @@ const LoaderSkeleton = (props: { count: number } = { count: 10 }) => {
           animate={true}
           style={{ width: '100%' }}
         >
-          <rect x="50" y="15" rx="5" ry="5" width="300" height="15" />
-          <rect x="50" y="39" rx="5" ry="5" width="220" height="9" />
-          <rect x="0" y="10" rx="0" ry="0" width="40" height="40" />
+          <rect key={0} x="50" y="15" rx="5" ry="5" width="300" height="15" />
+          <rect key={1} x="50" y="39" rx="5" ry="5" width="220" height="9" />
+          <rect key={2} x="0" y="10" rx="0" ry="0" width="40" height="40" />
         </ContentLoader>
       ))}
     />
