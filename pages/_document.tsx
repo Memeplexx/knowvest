e8657c5fe9707456e2cf3f900eb50fb0aa783f56
Stephen Paul <stephensimonpaul@gmail.com>
2023-08-30 09:54:33 +0200

    Fix SSR for styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,42 +1,71 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head
-        children={
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />), //gets the styles from all the components inside <App>
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
           <>
-            <link
-              rel="manifest"
-              href="/manifest.webmanifest"
-            />
-            <link
-              rel="apple-touch-icon"
-              href="/icon.png"
-            />
-            <meta
-              name="theme-color"
-              content="#000"
-            />
-            <meta
-              name="viewport"
-              content="user-scalable=yes, width=device-width, initial-scale=1.0"
-            />
-            <meta
-              name="apple-mobile-web-app-capable"
-              content="yes"
-            />
-            <meta
-              name="description"
-              content="Knowledge Harvest is a note taking app that helps you organize your thoughts and ideas."
-            />
+            {initialProps.styles}
+            {/*👇 insert the collected styles to the html document*/}
+            {sheet.getStyleElement()}
           </>
-        }
-      />
-      <body>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head
+          children={
+            <>
+              <link
+                rel="manifest"
+                href="/manifest.webmanifest"
+              />
+              <link
+                rel="apple-touch-icon"
+                href="/icon.png"
+              />
+              <meta
+                name="theme-color"
+                content="#000"
+              />
+              <meta
+                name="viewport"
+                content="user-scalable=yes, width=device-width, initial-scale=1.0"
+              />
+              <meta
+                name="apple-mobile-web-app-capable"
+                content="yes"
+              />
+              <meta
+                name="description"
+                content="Knowledge Harvest is a note taking app that helps you organize your thoughts and ideas."
+              />
+            </>
+          }
+        />
+        <body>
           <Main />
           <NextScript />
-      </body>
-    </Html>
-  )
+        </body>
+      </Html>
+    )
+  }
 }
+
