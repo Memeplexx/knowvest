c29eb4638c39a076df313239f0834bbada5d0a37
Stephen Paul <stephensimonpaul@gmail.com>
2023-09-07 12:49:52 +0200

    disable olik devtools on mobile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,18 @@ import type { AppProps } from "next/app"
 import type { Session } from "next-auth"
 import { augmentOlikForReact } from 'olik-react'
 import '@/utils/array';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
   augmentOlikForReact() // invoke before initializing store
-  useEffect(() => {
-    import('fastclick')
-      .then(({ default: FastClick }) => (FastClick as unknown as { attach: (arg: HTMLElement) => void }).attach(document.body))
-      .catch(console.error);
-  }, []);
+  // useEffect(() => {
+  //   import('fastclick')
+  //     .then(({ default: FastClick }) => (FastClick as unknown as { attach: (arg: HTMLElement) => void }).attach(document.body))
+  //     .catch(console.error);
+  // }, []);
   return (
     <SessionProvider session={session} >
       <Component {...pageProps} />
