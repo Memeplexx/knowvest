da6d85f79928e4c9bd1fdf628cbc9d4c3792e592
Memeplexx <stephensimonpaul@gmail.com>
2023-12-21 18:50:49 +0200

    Add user creation and note initialization logic

--- server/routers/session.ts
+++ server/routers/session.ts
@@ -18,15 +18,21 @@ export const sessionRouter = router({
       // Validate
       const userByEmail = await prisma.user.findFirst({ where: { email } });
 
-      // Create or update user
-      if (userByEmail) {
-        await prisma.user.update({ where: { email }, data: { email, image, name } });
-      } else {
-        await prisma.user.create({ data: { email, image, name } });
-      }
-      const user = await prisma.user.findFirstOrThrow({ where: { email } });
+      // If the user doesn't exist, create a new user and their first note
+      if (!userByEmail) {
+        const user = await prisma.user.create({ data: { email, image, name } });
+        const note = await prisma.note.create({
+          data: {
+            userId: user.id,
+            text: '# Welcome to Knowledge Harvest! ## This is your first note. Remove this text to get started ❤️',
+            dateViewed: new Date(),
+          }
+        });
+        return { status: 'SESSION_INITIALIZED_FOR_NEW_USER', user, note } as const;
 
-      const fetchData = async () => {
+      // Else if the user does exist, return their data after the specified date
+      } else {
+        const user = await prisma.user.update({ where: { email }, data: { email, image, name } });
         const userId = user.id;
         const dateUpdated = { gt: input.after || new Date(0) };
         const orderBy = { dateUpdated: 'desc' } as const;
@@ -36,21 +42,7 @@ export const sessionRouter = router({
         const noteTags = await prisma.noteTag.findMany({ where: { note: { userId }, dateUpdated }, orderBy });
         const synonymGroups = await prisma.synonymGroup.findMany({ where: { group: { userId }, dateUpdated }, orderBy });
         const tags = await prisma.tag.findMany({ where: { userId, dateUpdated }, orderBy });
-        return { notes, flashCards, groups, noteTags, synonymGroups, tags };
-      }
-
-      // If this is a new user, create their first note
-      if (!userByEmail) {
-        const note = await prisma.note.create({
-          data: {
-            userId: user!.id,
-            text: '# Welcome to Knowledge Harvest! ## This is your first note. Remove this text to get started ❤️',
-            dateViewed: new Date(),
-          }
-        });
-        return { status: 'SESSION_INITIALIZED_FOR_NEW_USER', user, note } as const;
-      } else {
-        return { status: 'SESSION_INITIALIZED', user, data: await fetchData() } as const;
+        return { status: 'SESSION_INITIALIZED', user, data: { notes, flashCards, groups, noteTags, synonymGroups, tags } } as const;
       }
     }),
 
