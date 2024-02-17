d3fe624d2849a1ebbeda62ead7b19f09e5ea70aa
Memeplexx <stephensimonpaul@gmail.com>
2024-02-17 16:21:27 +0200

    Update dependencies in package.json and pnpm-lock.yaml

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
  
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths() as any, react() as any], // todo: investigate alternative to casting
   test: {
     environment: 'jsdom',
   },
