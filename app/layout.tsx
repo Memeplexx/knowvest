4b18f55b289e6fd3468a4f20547fb7a664fe9454
Memeplexx <stephensimonpaul@gmail.com>
2024-02-18 21:31:00 +0200

    Refactor layout component and remove unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,31 +16,84 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en"
+
       children={
-        <body
-          suppressHydrationWarning={true}
-          children={
-            <>
-              <NextAuthProvider
-                children={
-                  <StoreProvider
-                    children={
-                      <NotifierProvider
-                        children={
-                          <StyledComponentsRegistry
-                            children={children}
-                          />
-                        }
-                      />
-                    }
-                  />
-                }
-              />
-              <SpeedInsights />
-              <Analytics />
-            </>
-          }
-        />
+        <>
+          <head
+            children={
+              <>
+                <title
+                  children='Knowledge Harvest'
+                />
+                <link
+                  rel="manifest"
+                  href="/manifest.webmanifest"
+                />
+                <link
+                  rel="apple-touch-icon"
+                  href="/icon.png"
+                />
+                <meta
+                  name="theme-color"
+                  content="#000"
+                />
+                <meta
+                  name="viewport"
+                  content="user-scalable=no, width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1"
+                />
+                <meta
+                  name="apple-mobile-web-app-capable"
+                  content="yes"
+                />
+                <meta
+                  name="description"
+                  content="Knowledge Harvest is a note taking app that helps you organize your thoughts and ideas."
+                />
+                <link
+                  rel="icon"
+                  href="/images/farm.svg"
+                  sizes="any"
+                />
+                <link
+                  rel="icon"
+                  href="/images/farm.svg?<generated>"
+                  type="image/<generated>"
+                  sizes="<generated>"
+                />
+                <link
+                  rel="apple-touch-icon"
+                  href="/images/farm.svg?<generated>"
+                  type="image/<generated>"
+                  sizes="<generated>"
+                />
+              </>
+            }
+          />
+          <body
+            suppressHydrationWarning={true}
+            children={
+              <>
+                <NextAuthProvider
+                  children={
+                    <StoreProvider
+                      children={
+                        <NotifierProvider
+                          children={
+                            <StyledComponentsRegistry
+                              children={children}
+                            />
+                          }
+                        />
+                      }
+                    />
+                  }
+                />
+                <SpeedInsights />
+                <Analytics />
+              </>
+            }
+          />
+        </>
       }
     />
   )
