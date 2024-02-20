880b1d563be831f2e841ec225fbe982f9679be13
Memeplexx <stephensimonpaul@gmail.com>
2024-02-20 15:47:55 +0200

    Delete unused hooks file

--- utils/hooks.ts
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-import { FunctionComponent, useEffect, useLayoutEffect, useMemo, useRef, useState, type ForwardedRef } from 'react';
-import { EventMap } from '../actions/types';
-
-
-
