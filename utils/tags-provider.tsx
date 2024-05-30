5b575c3e94149959ac72b7e644793f7f911caa6f
Memeplexx <stephensimonpaul@gmail.com>
2024-05-30 19:28:12 +0200

    Refactor TagsProvider to delay worker instantiation until state is initialized

--- utils/tags-provider.tsx
+++ utils/tags-provider.tsx
@@ -25,11 +25,11 @@ export default function TagsProvider({ children }: { children: React.ReactNode }
 
   useEffect(() => {
 
+    // Do not instantiate the worker until certain conditions are met
     if (!stateInitialized) return;
 
-    const worker = new Worker(new URL('./tags-worker.ts', import.meta.url)) as TagsWorker;
-
     // Configure object which will be passed to the consumer
+    const worker = new Worker(new URL('./tags-worker.ts', import.meta.url)) as TagsWorker;
     const eventListeners = new Array<Parameters<TagsWorkerHandler['addListener']>[0]>();
     const state: TagsWorkerHandler = {
       addListener(listener) {
@@ -46,10 +46,7 @@ export default function TagsProvider({ children }: { children: React.ReactNode }
     setState(state);
 
     // Ensure that tags are in sync with the worker
-    worker.postMessage({
-      type: 'addTags',
-      data: store.$state.tags.map(t => ({ id: t.id, text: t.text, synonymId: t.synonymId })),
-    });
+    state.addTags(store.$state.tags.map(t => ({ id: t.id, text: t.text, synonymId: t.synonymId })));
     const unsubscribeFromTagsChange = store.tags.$onChange((tags, previousTags) => {
       const previousTagIds = previousTags.map(t => t.id);
       const tagsToAdd = tags.filter(t => !previousTagIds.includes(t.id));
